42fa34500bfdb86c898a5e0d67e199e06713c8ab
Mike <mike@mikera.net>
2018-09-28 00:38:42 +0800

    Handler updates for better asset listing

--- src/main/clojure/surfer/handler.clj
+++ src/main/clojure/surfer/handler.clj
@@ -5,7 +5,15 @@
     [ring.middleware.format :refer [wrap-restful-format]]
     [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
     [surfer.store :as store]
+    [clojure.data.json :as json]
     [surfer.ckan :as ckan]))
+
+(defn json-response
+  [data & [status]]
+  (let [status (or status 200)]
+    {:status status
+     :headers {"Content-Type" "application/json"}
+     :body data}))
 
 (defroutes routes
   (GET "/" [] "Welcome.")
@@ -16,14 +24,17 @@
               (apply str 
                      (mapv 
                        (fn [id]
-                         (str "<a href=\"/metadata/" id "\">" id " <br/>\n"))
+                         (let [j (json/read-str (store/lookup id))
+                               title (j "title")]
+                           (str "<a href=\"/metadata/" id "\">" id " - " title "<br/>\n")))
                        (store/all-keys)))
               "</body>"
               ))
   
   (GET "/metadata/:id" [id] 
-       (or (store/lookup id)
-           (route/not-found "404")))
+       (if-let [js (store/lookup id)]
+         (json-response js)
+         (route/not-found "404")))
   
   (route/not-found "404"))
 
