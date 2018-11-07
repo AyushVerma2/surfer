f45e2e1a358d5ac92de4e38d6a3d2963498a8685
Mike <mike@mikera.net>
2018-11-07 13:14:23 +0800

    Extra keccak256 test case

--- src/test/clojure/surfer/test_utils.clj
+++ src/test/clojure/surfer/test_utils.clj
@@ -4,4 +4,8 @@
 
 (deftest test-keccak256
   (is (= "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
-         (hex-string (keccak256 "")))))
+         (hex-string (keccak256 ""))))
+  
+  (is (= "4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45"
+         (hex-string (keccak256 "abc")))))
+
