f82147efce798af9642dda5919547f3f2c8a380f
Mike <mike@mikera.net>
2018-11-07 12:51:01 +0800

    Basic H2 embedded database

--- src/main/clojure/surfer/store.clj
+++ src/main/clojure/surfer/store.clj
@@ -1,6 +1,21 @@
 (ns surfer.store
   (:require [surfer.utils :as u]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.java.jdbc :as jdbc]))
+
+(def db {:dbtype "h2"
+         :dbname "~/surfertest"})
+
+(Class/forName "org.h2.Driver")
+
+(defn create-db! [db] 
+  (jdbc/execute! db 
+  "CREATE TABLE IF NOT EXISTS Metadata ( 
+     id CHAR(64) NOT NULL PRIMARY KEY, 
+     metadata CLOB NOT NULL, 
+     utime TIMESTAMP NOT NULL
+   );"
+  ))
 
 (defonce database (atom {}))
 
