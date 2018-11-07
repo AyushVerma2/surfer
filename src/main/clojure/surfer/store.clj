fc2a6d605a7121b29dc4c0dbec22e70f729110df
Mike <mike@mikera.net>
2018-11-07 13:34:07 +0800

    Move metadata to H2 data store

--- src/main/clojure/surfer/store.clj
+++ src/main/clojure/surfer/store.clj
@@ -1,7 +1,8 @@
 (ns surfer.store
   (:require [surfer.utils :as u]
             [clojure.data.json :as json]
-            [clojure.java.jdbc :as jdbc]))
+            [clojure.java.jdbc :as jdbc])
+  (:import [java.time LocalDateTime]))
 
 (def db {:dbtype "h2"
          :dbname "~/surfertest"})
@@ -12,20 +13,35 @@
   (jdbc/execute! db 
   "CREATE TABLE IF NOT EXISTS Metadata ( 
      id CHAR(64) NOT NULL PRIMARY KEY, 
-     metadata CLOB NOT NULL, 
+     metadata varchar NOT NULL, 
      utime TIMESTAMP NOT NULL
    );"
   ))
 
+(defn drop-db! [db]
+  (jdbc/execute! db 
+  "drop TABLE Metadata;"
+  ))
+
+(create-db! db)
+
 (defonce database (atom {}))
 
-(defn register [asset-metadata-str]
-  (let [hash (u/hex-string (u/sha256 asset-metadata-str))]
-    (swap! database assoc hash asset-metadata-str)
+(defn register [^String asset-metadata-str]
+  (let [hash (u/hex-string (u/keccak256 asset-metadata-str))
+        rs (jdbc/query db ["select * from Metadata where id = ?" hash])]
+    (if (empty? rs)
+      (jdbc/insert! db "Metadata" 
+                    {:id hash 
+                     :metadata asset-metadata-str
+                     :utime (LocalDateTime/now)}))
     hash))
 
-(defn lookup [id-str]
-  (get @database id-str))
+(defn lookup [^String id-str]
+  (let [rs (jdbc/query db ["select * from Metadata where id = ?" id-str])]
+    (if (empty? rs)
+      nil
+      (str (:metadata (first rs))))))
 
 (defn all-keys []
   (keys @database))
