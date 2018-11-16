0c7df0c7f464f65180ebe262566f58a6ace66f13
Mike <mike@mikera.net>
2018-11-16 11:38:45 +0800

    Tests for requesting non-existent assets

--- src/test/clojure/surfer/test_handler.clj
+++ src/test/clojure/surfer/test_handler.clj
@@ -2,6 +2,9 @@
   (:require 
     [surfer systems]
     [clj-http.client :as client]
+    [clojure.data.json :as json]
+    [surfer.utils :as utils]
+    [slingshot.slingshot :refer [try+ throw+]]
     [surfer.systems :refer [base-system]]
     [system.repl :refer [set-init! go start reset stop]]
     [cemerick.friend [workflows :as workflows]
@@ -22,8 +25,31 @@
   (is (= 200 (:status (client/get (str BASE_URL) AUTH_HEADERS)))))
 
 (deftest test-register-upload
-  (let [r1 (client/post (str BASE_URL "api/v1/meta/data") AUTH_HEADERS)]
-    (is (= 200 (:status r1))))
-  )
+  (let [r1 (client/post (str BASE_URL "api/v1/meta/data") AUTH_HEADERS)
+        id (json/read-str (:body r1))]
+    (is (= 200 (:status r1)))
+    (is (utils/valid-asset-id? id))
+    (let [r2 (client/get (str BASE_URL "api/v1/meta/data/" id) AUTH_HEADERS)]
+      (is (= 200 (:status r1)))
+      (println r2)
+      
+      )
+    ))
+
+(deftest test-no-asset 
+  (try+ 
+      (client/get (str BASE_URL "api/v1/meta/data/" 
+                       "000011112222333344445556666777788889999aaaabbbbccccddddeeeeffff") AUTH_HEADERS)
+      (catch [:status 404] {:keys [request-time headers body]}
+        ;; OK, not found expected
+        )
+      )
+  (try+ 
+      (client/get (str BASE_URL "api/v1/meta/data/" 
+                       "0000") AUTH_HEADERS)
+      (catch [:status 404] {:keys [request-time headers body]}
+        ;; OK, not found expected
+        )
+      ))
 
 
