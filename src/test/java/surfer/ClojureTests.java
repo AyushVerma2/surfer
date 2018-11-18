2d71c4d1001831492a7f40edde6a52936f270dd0
Mike <mike@mikera.net>
2018-11-18 10:59:15 +0800

    Clojuretests docs

--- src/test/java/surfer/ClojureTests.java
+++ src/test/java/surfer/ClojureTests.java
@@ -2,6 +2,13 @@ package surfer;
 
 import mikera.cljunit.ClojureTest;
 
+/**
+ * This class implements a JUnit test interface enabling the 
+ * Clojure test suite to be run using standard JUnit tooling.
+ * 
+ * @author Mike
+ *
+ */
 public class ClojureTests extends ClojureTest {
 	
 	@Override
