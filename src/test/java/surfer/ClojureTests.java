0148bcbdd1103d483c1c1449d3b3ffefd0942b05
Mike <mike@mikera.net>
2019-01-25 14:49:59 +0800

    Update for surefire test reports

--- src/test/java/surfer/ClojureTests.java
+++ src/test/java/surfer/ClojureTests.java
@@ -3,17 +3,17 @@ package surfer;
 import mikera.cljunit.ClojureTest;
 
 /**
- * This class implements a JUnit test interface enabling the 
- * Clojure test suite to be run using standard JUnit tooling.
+ * This class implements a JUnit test interface enabling the Clojure test suite
+ * to be run using standard JUnit tooling.
  * 
  * @author Mike
  *
  */
 public class ClojureTests extends ClojureTest {
-	
+
 	@Override
 	public String filter() {
 		return "surfer";
 	}
-	
+
 }
