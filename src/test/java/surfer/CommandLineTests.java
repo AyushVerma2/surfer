e6c0adf993c29a2ed79495b6cbe41a7681c3a527
Tom Marble <tmarble@info9.net>
2019-02-27 14:20:06 -0600

    Adds test coverage for mvn test
    Configures ClojureTests to call specific Clojure tests
    Updates POM:
    - changes groupId to com.DEX-Company
    - changes scm references DEX-Company
    - disables testFailureIgnore on maven-surefire-plugin so failing
      tests result in a non-zero exit code
    Adds infrastructure for running command line tests via JUnit CommandLineTests
    - currently disabled due to interaction with surefire

---
+++ src/test/java/surfer/CommandLineTests.java
@@ -0,0 +1,40 @@
+package surfer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class CommandLineTests {
+
+	@Test public void testCommandLine() throws IOException, InterruptedException {
+		List<String> commands = new ArrayList<String>();
+		// commands.add("./test/bin/cli-test");
+		commands.add("/bin/true");
+		ProcessBuilder pb = new ProcessBuilder(commands);
+		File cwd = new File(System.getProperty("user.dir"));
+		pb.directory(cwd);
+		pb.redirectErrorStream(true);
+		Process process = pb.start();
+		StringBuilder out = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		String line = null, previous = null;
+		while ((line = br.readLine()) != null) {
+			previous = line;
+			out.append(line).append('\n');
+		}
+
+		if (process.waitFor() == 0) {
+			// assertEquals("0 failures",previous);
+		} else {
+			fail("command line tests failed:\n" + out.toString());
+		}
+	}
+}
