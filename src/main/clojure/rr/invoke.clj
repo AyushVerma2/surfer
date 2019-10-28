3199505ddf065e9fcbc605a9965b2f0d1cb345bb
Mike <mike@mikera.net>
2019-10-28 09:53:03 +0800

    Working RR test invoke

--- src/main/clojure/rr/invoke.clj
+++ src/main/clojure/rr/invoke.clj
@@ -1,7 +1,18 @@
 (ns rr.invoke
   (:use [clojure.repl])
   (:require [clojure.test :refer :all])
-  (:require [starfish.core :as sf]))
+  (:require [starfish.core :as sf]
+            [surfer.config :as config]))
+
+(def local-did
+  config/DID)
+
+(def local-ddo
+  config/LOCAL-DDO)
+
+(def local-ddo-string (sf/json-string-pprint local-ddo))
+
+(def ag (sf/remote-agent local-did local-ddo-string "Aladdin" "OpenSesame"))
 
 (defn test-function
   "Sample function to invoke with JSON"
@@ -22,5 +33,6 @@
 (defn setup-invoke 
   "Sets up a test operation"
   []
-  (let [] 
-    {:status "OK"}))
+  (let [op (sf/register ag (sf/memory-asset op-meta-map ""))] 
+    {:status "OK"
+     :ops [(str op)]}))
