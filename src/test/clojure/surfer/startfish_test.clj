88cebe6dbb9b9954530f46cd1e4c2679ae903bd1
Pedro Girardi <pedro.girardi@dex.sg>
2019-11-01 16:39:10 +0700

    Always stop system; Assert asset metadata

--- src/test/clojure/surfer/startfish_test.clj
+++ src/test/clojure/surfer/startfish_test.clj
@@ -5,22 +5,47 @@
             [surfer.config :as config]
             [starfish.core :as sf]))
 
+(defn test-function-1
+  "Sample function to invoke"
+  [inputs]
+  (let [_ (println (str "function received:" inputs))
+        a (sf/asset (:input inputs))
+        ^String c (sf/to-string a)
+        C (.toUpperCase c)]
+    {:output (sf/memory-asset {:name "Result of computation"} C)}))
+
 (deftest ^:integration test-startfish
-  (let [system (component/start-system (systems/base-system))
+  (let [system (component/start-system (systems/base-system))]
+    (try
+      (let [local-did config/DID
+            local-ddo config/LOCAL-DDO
+            local-ddo-string (sf/json-string-pprint local-ddo)
 
-        local-did config/DID
-        local-ddo config/LOCAL-DDO
-        local-ddo-string (sf/json-string-pprint local-ddo)
+            username "Aladdin"
+            password "OpenSesame"
 
-        username "Aladdin"
-        password "OpenSesame"
+            agent (sf/remote-agent local-did local-ddo-string username password)
 
-        remote-agent (sf/remote-agent local-did local-ddo-string username password)
+            foo-memory-asset (sf/memory-asset "Foo")
+            foo-remote-data-asset (sf/upload agent foo-memory-asset)]
 
-        memory-asset (sf/memory-asset "Foo")
-        remote-data-asset (sf/upload remote-agent memory-asset)]
+        (is (= (sf/to-string (sf/content foo-memory-asset))
+               (sf/to-string (sf/content foo-remote-data-asset))))
 
-    (is (= (sf/to-string (sf/content memory-asset))
-           (sf/to-string (sf/content remote-data-asset))))
+        (is (= (sf/metadata-string foo-memory-asset)
+               (sf/metadata-string foo-remote-data-asset)))
 
-    (component/stop-system system)))
+        ;; * FAILS *
+        ;;(let [operation (->> (sf/memory-asset {:name "Test operation"
+        ;;                                       :type "operation"
+        ;;                                       :operation {:params {:input {:type "asset"}}
+        ;;                                                   :results {:output {:type "asset"}}}
+        ;;                                       :additionalInfo {:function "surfer.startfish-test/test-function-1"}} "")
+        ;;                     (sf/register agent))
+        ;;
+        ;;      result (sf/invoke-sync operation {:input foo-remote-data-asset})]
+        ;;  (prn result))
+
+        )
+      (finally
+        (component/stop-system system)))))
