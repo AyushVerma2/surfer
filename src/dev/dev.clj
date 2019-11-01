680b173b555a16f155fb30d67fa89a01dcd3f22e
Pedro Girardi <pedro.girardi@dex.sg>
2019-11-01 11:54:19 +0700

    Dev workflow with Clojure CLI / deps.edn

---
+++ src/dev/dev.clj
@@ -0,0 +1,8 @@
+(ns dev
+  (:require [surfer.store :as store]
+            [surfer.config :as config]
+            [surfer.systems :as systems]
+            [system.repl :refer [set-init! go reset start stop]]))
+
+(set-init! #'systems/base-system)
+
