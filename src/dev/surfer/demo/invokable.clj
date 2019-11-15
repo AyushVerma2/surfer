249e2fa0b61428187063f643732ae613523f43dd
Pedro Girardi <pedro.girardi@dex.sg>
2019-11-15 18:47:18 +0700

    TODO

--- src/dev/surfer/demo/invokable.clj
+++ src/dev/surfer/demo/invokable.clj
@@ -9,6 +9,19 @@
 (def operation-odd?
   (sf/in-memory-operation (sf/invokable-metadata #'invokable-odd?)))
 
+;; TODO
+(defn invokable-asset-odd? [params]
+  ;; Asset -> Map
+  (let [n 1]
+    {:n n
+     :odd? (odd? n)}))
+
+(defn invokable-asset2-odd? [params]
+  ;; Asset -> Asset
+  (let [n 1]
+    {:n n
+     :odd? (odd? n)}))
+
 (defn invokable-inc [params]
   (update params :n inc))
 
