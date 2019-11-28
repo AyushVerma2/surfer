6cda74b297a350d1f8a47be5e0b7e01c58ae4a24
Pedro Girardi <pedro@plando.work>
2019-11-28 17:24:13 +0700

    Storage tests

--- src/test/clojure/surfer/store_test.clj
+++ src/test/clojure/surfer/store_test.clj
@@ -3,7 +3,8 @@
             [surfer.store :as store]
             [surfer.system :as system]
             [surfer.database :as database]
-            [surfer.test.fixture :as fixture]))
+            [surfer.test.fixture :as fixture]
+            [starfish.core :as sf]))
 
 (def test-system
   nil)
@@ -11,6 +12,14 @@
 (use-fixtures :once (fixture/system-fixture #'test-system))
 
 (deftest test-metadata-index
-  (let [db (database/db (system/h2 test-system))]
-    (is (= (into #{} (store/all-keys db))
-           (into #{} (keys (store/metadata-index db)))))))
+  (testing "Metadata Index Keys"
+    (let [db (database/db (system/h2 test-system))]
+      (is (= (into #{} (store/all-keys db))
+             (into #{} (keys (store/metadata-index db))))))))
+
+(deftest test-content-hash
+  (testing "Memory Asset Content Hash"
+    (let [data "Hello"
+          sha (sf/digest data)
+          asset (sf/memory-asset {:contentHash sha} data)]
+      (is (= sha (sf/digest asset))))))
