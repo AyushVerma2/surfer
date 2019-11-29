0abb41fa6845cd1263b5762d6ab35cfae130418b
Pedro Girardi <pedro@plando.work>
2019-11-29 18:04:22 +0700

    More tests

--- src/test/clojure/starfish/alpha_test.clj
+++ src/test/clojure/starfish/alpha_test.clj
@@ -3,7 +3,8 @@
             [starfish.alpha :as sfa]
             [starfish.core :as sf])
   (:import (sg.dex.starfish.impl.remote RemoteAccount)
-           (sg.dex.starfish.impl.memory MemoryAgent LocalResolverImpl)))
+           (sg.dex.starfish.impl.memory MemoryAgent LocalResolverImpl)
+           (sg.dex.starfish Resolver)))
 
 (defmethod sfa/resolve-agent "1acd41655b2d8ea3f3513cc847965e72c31bbc9bfc38e7e7ec901852bd3c457c" [resolver did ddo]
   (MemoryAgent/create resolver did))
@@ -17,14 +18,30 @@
     (let [credentials (.getCredentials ^RemoteAccount (sfa/remote-account "x"))]
       (is (= #{"token"} (set (keys credentials)))))))
 
+(deftest register!-test
+  (testing "Register DID & DDO"
+    (let [local-resolver (LocalResolverImpl.)
+          did (sf/did "did:dex:1acd41655b2d8ea3f3513cc847965e72c31bbc9bfc38e7e7ec901852bd3c457c")]
+
+      (testing "Able to get DDO"
+        (binding [sfa/*resolver* local-resolver]
+          (sfa/register! did {:name "Surfer"})
+          (is (= {"name" "Surfer"} (.getDDO ^Resolver sfa/*resolver* did)))
+          (is (= {"name" "Surfer"} (.getDDO ^Resolver local-resolver did)))))
+
+      (testing "Unable to get DDO"
+        (is (= nil (.getDDO ^Resolver sfa/*resolver* did)))))))
+
 (deftest did->agent-test
   (testing "Resolve Agent"
     (let [did (sf/did "did:dex:1acd41655b2d8ea3f3513cc847965e72c31bbc9bfc38e7e7ec901852bd3c457c")]
 
       (testing "Able to resolve"
-        (binding [sfa/*resolver* (LocalResolverImpl.)]
-          (sfa/register! did {})
-          (is (sfa/did->agent did))))
+        (let [local-resolver (LocalResolverImpl.)]
+          (binding [sfa/*resolver* local-resolver]
+            (sfa/register! did {})
+            (is (sfa/did->agent did))
+            (is (sfa/did->agent local-resolver did)))))
 
       (testing "Unable to resolve"
         (binding [sfa/*resolver* (LocalResolverImpl.)]
