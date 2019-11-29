f4ecf05208df8ed39e1c55821f4f6c4d7584f624
Pedro Girardi <pedro@plando.work>
2019-11-29 17:13:44 +0700

    Starfish API - remote-account

--- src/main/clojure/starfish/alpha.clj
+++ src/main/clojure/starfish/alpha.clj
@@ -2,7 +2,10 @@
   (:require [starfish.core :as sf]
             [clojure.data.json :as data.json])
   (:import (sg.dex.starfish Resolver)
-           (sg.dex.starfish.impl.memory LocalResolverImpl)))
+           (sg.dex.starfish.impl.memory LocalResolverImpl)
+           (sg.dex.starfish.impl.remote RemoteAccount)
+           (sg.dex.starfish.util Utils)
+           (java.util HashMap Map)))
 
 (def ^:dynamic *resolver*
   (LocalResolverImpl.))
@@ -27,3 +30,14 @@
    (when-let [ddo-str (.getDDOString resolver did)]
      (let [ddo (data.json/read-str ddo-str :key-fn str)]
        (resolve-agent resolver did ddo)))))
+
+(defn remote-account
+  ([token]
+   (let [^Map credentials (doto (HashMap.)
+                            (.put "token" token))]
+     (RemoteAccount/create (Utils/createRandomHexString 32) credentials)))
+  ([username password]
+   (let [^Map credentials (doto (HashMap.)
+                            (.put "username" username)
+                            (.put "password" password))]
+     (RemoteAccount/create (Utils/createRandomHexString 32) credentials))))
