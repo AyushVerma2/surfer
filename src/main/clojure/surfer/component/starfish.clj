88a84368710059bd5432ac993352f49a0042986d
Pedro Girardi <pedro@plando.work>
2019-12-04 08:22:40 +0700

    Use starfish.core/remote-account

--- src/main/clojure/surfer/component/starfish.clj
+++ src/main/clojure/surfer/component/starfish.clj
@@ -1,5 +1,6 @@
 (ns surfer.component.starfish
-  (:require [starfish.alpha :as sfa]
+  (:require [starfish.core :as sf]
+            [starfish.alpha :as sfa]
             [surfer.env :as env]
             [com.stuartsierra.component :as component])
   (:import (sg.dex.starfish.impl.remote RemoteAgent)))
@@ -20,5 +21,5 @@
 (defmethod sfa/resolve-agent "1acd41655b2d8ea3f3513cc847965e72c31bbc9bfc38e7e7ec901852bd3c457c" [resolver did ddo]
   (let [username (get-in ddo ["credentials" "username"])
         password (get-in ddo ["credentials" "password"])
-        account (sfa/remote-account username password)]
+        account (sf/remote-account username password)]
     (RemoteAgent/create resolver did account)))
