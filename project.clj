582df0d0e2af43f4f7b48377bb8a90168c0ec5f9
Pedro Girardi <pedro@plando.work>
2019-12-06 09:09:15 +0700

    Trigger deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sg.dex/surfer "0.1.0-SNAPSHOT"
+(defproject sg.dex/surfer "0.2.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"])
