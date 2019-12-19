22470eb8db9907dfe47d2ed1998e2d8f9c44f836
Pedro Girardi <pedro@plando.work>
2019-12-19 09:08:15 +0700

    Revert "Surfer name per env - dev, test, prod"
    This reverts commit 0fa8ef79d997ac9acfe96b8031b352563131f141.

--- src/main/clojure/surfer/starfish.clj
+++ src/main/clojure/surfer/starfish.clj
@@ -18,20 +18,8 @@
   (stop [component]
     component))
 
-(defn- surfer [resolver did ddo]
+(defmethod sfa/resolve-agent "1acd41655b2d8ea3f3513cc847965e72c31bbc9bfc38e7e7ec901852bd3c457c" [resolver did ddo]
   (let [username (get-in ddo ["credentials" "username"])
         password (get-in ddo ["credentials" "password"])
         account (sf/remote-account username password)]
     (RemoteAgent/create resolver did account)))
-
-;; -- dev
-(defmethod sfa/resolve-agent "1acd41655b2d8ea3f3513cc847965e72c31bbc9bfc38e7e7ec901852bd3c457c" [resolver did ddo]
-  (surfer resolver did ddo))
-
-;; -- test
-(defmethod sfa/resolve-agent "1acd41655b2d8ea3f3513cc847965e72c31bbc9bfc38e7e7ec901852bd3c457d" [resolver did ddo]
-  (surfer resolver did ddo))
-
-;; -- prod
-(defmethod sfa/resolve-agent "1acd41655b2d8ea3f3513cc847965e72c31bbc9bfc38e7e7ec901852bd3c457e" [resolver did ddo]
-  (surfer resolver did ddo))
