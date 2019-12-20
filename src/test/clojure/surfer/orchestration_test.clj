5850d1076ca170da4e95065a02442d16bf7e37a9
Pedro Girardi <pedro@plando.work>
2019-12-20 15:37:18 +0700

    Tests - dependency-edges

--- src/test/clojure/surfer/orchestration_test.clj
+++ src/test/clojure/surfer/orchestration_test.clj
@@ -25,3 +25,26 @@
     (is (= {"C" #{"B" "A"}, "D" #{"C"}} (:dependencies graph)))
     (is (= {"A" #{"C"}, "B" #{"C"}, "C" #{"D"}} (:dependents graph)))
     (is (= ["A" "B" "C" "D"] (dep/topo-sort graph)))))
+
+(deftest dependency-edges-test
+  (let [orchestration {:edges
+                       [{:source "a"
+                         :target "b"}
+
+                        {:source "b"
+                         :target "c"}
+
+                        {:source "make-range"
+                         :target "concatenate"}
+
+                        {:source "make-range"
+                         :target "concatenate"}]}
+
+        edges (orchestration/dependency-edges orchestration "concatenate" "make-range")]
+    (is (= 2 (count edges)))
+
+    (is (= #{"make-range"} (->> (map :source edges)
+                                (into #{}))))
+
+    (is (= #{"concatenate"} (->> (map :target edges)
+                                 (into #{}))))))
