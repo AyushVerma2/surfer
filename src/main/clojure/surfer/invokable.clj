a6b58d9fdd7d697adf578878c248882f8bbf3c98
Pedro Girardi <pedro@plando.work>
2020-01-20 17:40:47 +0700

    Clean up

--- src/main/clojure/surfer/invokable.clj
+++ src/main/clojure/surfer/invokable.clj
@@ -30,6 +30,10 @@
     (->> (wrapped-params metadata params)
          (invokable app-context))))
 
+(defn format-asset-result
+  [^DID agent-did asset]
+  {:did (str (.withPath agent-did (sf/asset-id asset)))})
+
 ;; TODO: More thinking
 ;; Don't need 'asset-fn' - it's up to the user.
 ;; When should the Asset be uploaded?
@@ -56,8 +60,6 @@
   (fn [app-context params]
     (->> (invokable app-context params)
          (wrapped-results metadata))))
-
-(defonce JOBS (atom {}))
 
 (defn resolve-invokable [metadata]
   (some-> (get-in metadata [:additionalInfo :function])
@@ -106,81 +108,3 @@
         operation (invokable-operation context metadata)]
     (sf/invoke-result operation params)))
 
-(defn get-operation
-  "Gets an in-memory operation for the given operation id"
-  [db op-id]
-  (let [op-meta (store/get-metadata-str db op-id)
-        md (sf/read-json-string op-meta)
-        op-function (:function (:additionalInfo md))
-        params (keys (:params (:operation md)))
-        op-sym (symbol op-function)
-        f (resolve op-sym)]
-    (when f (sf/create-operation params f))))
-
-(defn coerce-input-params
-  "Coerces the input request to a map of keywords to assets / objects"
-  ([md req]
-   (let [pspecs (:params (:operation md))]
-     (reduce
-       (fn [m [k v]]
-         (if-let [pspec (get pspecs (keyword k))]
-           (let [type (:type pspec)]
-             (if (= "asset" type)
-               (assoc m (keyword k) (sf/asset (:did v)))
-               (assoc m (keyword k) v)))
-           m))
-       req
-       req))))
-
-(defn launch-job
-  "Launch a job using the given function. 
-
-   Returns jobid if successful, null if operation cannot be found."
-  [db op-id invoke-req]
-  (let [op (get-operation db op-id)]
-    (when op
-      (let [jobid (sf/random-hex-string 32)
-            md (sf/metadata op)
-            job (sf/invoke op (coerce-input-params md invoke-req))]
-        (swap! JOBS assoc jobid job)
-        (log/debug (str "Job started with ID [" jobid "]"))
-        jobid))))
-
-(defn get-job
-  "Gets the job for a given Job ID"
-  [jobid]
-  (@JOBS jobid))
-
-(defn format-asset-result
-  [^DID agent-did asset]
-  {:did (str (.withPath agent-did (sf/asset-id asset)))})
-
-(defn format-results
-  "Formats results ready for output in a job status :result value."
-  [^DID agent-did rs]
-  (into {}
-        (map (fn [[k v]]
-               (if (sf/asset? v)
-                 [(keyword k) (format-asset-result agent-did v)]
-                 [(keyword k) v]))
-             rs)))
-
-(defn job-response
-  "Gets the appropriate response map for a job result, or null if the job does not exist."
-  [app-context job]
-  (let [agent-did (env/self-did (app-context/env app-context))
-
-        _ (try
-            (sf/poll-result job)
-            (catch Throwable _))
-
-        status (sf/job-status job)
-        resp {:status status}
-        resp (if (= status :succeeded)
-               (assoc resp :results (format-results agent-did (sf/get-result job)))
-               resp)
-        resp (if (= status :failed)
-               (assoc resp :message (try (sf/get-result job)
-                                         (catch Throwable t (.getMessage t))))
-               resp)]
-    resp))
